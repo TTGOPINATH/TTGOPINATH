a34d406ce62af350619d5a5d4e3fdb14bfa979f7
TTGOPINATH <161149817+TTGOPINATH@users.noreply.github.com>
2024-02-28 21:03:29 +0530

    Add files via upload

---
+++ operator.java
@@ -0,0 +1,19 @@
+public class operator
+{
+    public static void main(String args[])
+    {
+        int a=10;
+        int b=20;
+        int add=a+b;
+        int sub=a-b;
+        int div=a/b;
+        int mod=a%b;
+        int mult=a*b;
+        operator o=new operator();
+        System.out.println("addition is:"+add);
+        System.out.println("subtraction is:"+sub);
+        System.out.println("division is:"+div);
+        System.out.println("modulus is:"+mod);
+        System.out.println("multiplication is:"+mult);
+    }
+}
